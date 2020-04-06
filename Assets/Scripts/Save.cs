4792946b66f6c22edf796c4ca70b9b1c9fc6b98f
Cube <35161393+cubeww@users.noreply.github.com>
2020-04-07 07:59:42 +0800

    new animator test

---
+++ Assets/Scripts/Save.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Save : MonoBehaviour
+{
+    
+}
---
+++ Assets/Scripts/SpriteAnimator.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System;
+
+public class SpriteAnimator : MonoBehaviour
+{
+    public SpriteAnimation[] animations;
+
+    public string startAnimation;
+
+    string _currentAnimation;
+
+    string currentAnimation
+    {
+        set
+        {
+            _currentAnimation = value;
+            foreach (var i in animations)
+            {
+                if (i.name == _currentAnimation)
+                    animation = i;
+            }
+        }
+        get
+        {
+            return _currentAnimation;
+        }
+    }
+
+    SpriteAnimation animation;
+
+    public float imageSpeed;
+    public float imageIndex;
+
+    SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        currentAnimation = startAnimation;
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        imageIndex += imageSpeed;
+        
+        spriteRenderer.sprite = animation.sprites[(int)imageIndex % animation.sprites.Length];
+    }
+}
+
+[Serializable]
+public class SpriteAnimation
+{
+    public string name;
+    public Sprite[] sprites;
+}
+
+
